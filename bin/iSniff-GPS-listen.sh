b7843dd69be770f4a19b3e8b7c757cfa13d1f7ac
Peter Reuterås <peter@reuteras.net>
2016-02-17 12:07:51 +0100

    No need for info. Added script.

--- bin/iSniff-GPS-listen.sh
+++ bin/iSniff-GPS-listen.sh
@@ -4,7 +4,5 @@ ifconfig wlan0 down
 iwconfig wlan0 mode monitor
 airmon-ng start wlan0
 cd /root/src/iSniff-GPS
-echo "Run the command below in a separate window:"
-echo "cd /root/src/iSniff-GPS && . .env/bin/activate && ./manage.py runserver 127.0.0.1:8000"
 . .env/bin/activate && ./run.sh -i wlan0
 
