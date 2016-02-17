b3d627883445e7f16051700c7f293219a8d8465f
Peter Reuterås <peter@reuteras.net>
2016-02-17 11:58:10 +0100

    Use virtualenv.

--- bin/iSniff-GPS.sh
+++ bin/iSniff-GPS.sh
@@ -5,6 +5,6 @@ iwconfig wlan0 mode monitor
 airmon-ng start wlan0
 cd /root/src/iSniff-GPS
 echo "Run the command below in a separate window:"
-echo "cd /root/src/iSniff-GPS && ./manage.py runserver 127.0.0.1:8000"
-./run.sh -i wlan0
+echo "cd /root/src/iSniff-GPS && . .env/bin/activate && ./manage.py runserver 127.0.0.1:8000"
+. .env/bin/activate && ./run.sh -i wlan0
 
