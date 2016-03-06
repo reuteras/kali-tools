18e4354f287899a096950f09c537e17ac40b1a74
Peter Reuterås <peter@reuteras.com>
2016-03-06 23:48:07 +0100

    Start iceweasel.

--- bin/iSniff-GPS-web.sh
+++ bin/iSniff-GPS-web.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 cd /root/src/iSniff-GPS && . .env/bin/activate && \
-    ./manage.py runserver 127.0.0.1:8000
+    ./manage.py runserver 127.0.0.1:8000 && \
+    iceweasel http://127.0.0.1:8000
 
