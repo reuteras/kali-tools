ee6bcf7a30a46ae6848687fa19bd239fe68047cf
Peter Reuterås <peter@reuteras.com>
2016-03-13 21:03:24 +0100

    Install exfat support and libbde.

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+
 apt-get update && apt-get -y dist-upgrade
 
+# Install Vmware tools
 apt-get install -y open-vm-tools-desktop fuse
 
+# Install tools
+apt-get install -y libbde-dev libbde-utils exfat-fuse exfat-utils
 
