a2d33116b14c4b63d72d006519f62f7036118e6c
Peter Reuterås <peter@reuteras.com>
2016-12-10 08:45:04 +0100

    Add ducktoolkit

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 apt-get update && apt-get -y dist-upgrade
 
 # Install Vmware tools
@@ -9,3 +8,9 @@ apt-get install -y open-vm-tools-desktop fuse
 # Install tools
 apt-get install -y libbde-dev libbde-utils exfat-fuse exfat-utils
 
+# Make sure pip is up to date
+pip install --upgrade pip
+
+# Install tool for Rubber ducky
+# Resources http://usbrubberducky.com/#!resources.md
+pip install --upgrade ducktoolkit
