db140ce5c671307de2179940c1378c2b7ca6400d
Peter Reuterås <peter@reuteras.com>
2018-11-25 19:25:05 +0100

    Added check for memory/swap. Needs testing.

--- bin/install-kismet-source.sh
+++ bin/install-kismet-source.sh
@@ -10,6 +10,26 @@ EOF
     systemctl restart NetworkManager
 fi
 
+# Couldn't compile on Rasberry Pi
+MEMORY=$(free | grep Mem: | awk '{print $2}' )
+SWAP=$(free | grep Swap: | awk '{print $2}' )
+CREATEDSWAP=0
+
+if [[ $MEMORY -lt 2097152 ]]; then
+	echo "Memory smaller then 2GB. Checking swap."
+    if [[ $SWAP -lt 1048576 ]]; then
+    	echo "Creating swap in /root/swap."
+        if [[ -e /root/swap ]]; then
+ 			echo "/root/swap exists!"
+            exit
+        fi
+        dd if=/dev/zero of=/root/swap bs=1024 count=1048576
+        chmod 0600 /root/swap
+        mkswap /root/swap
+        swapon /root/swap
+		CREATEDSWAP=1
+	fi
+fi
 
 apt update && apt upgrade -yqq
 apt install -yqq \
@@ -46,3 +66,9 @@ make plugins
 make plugins-install
 make restricted-plugins
 make restricted-plugins-install
+
+if [[ "$CREATEDSWAP" == "1" ]]; then
+	swapoff /root/swap
+	rm /root/swap
+fi
+
--- bin/update-kismet-source.sh
+++ bin/update-kismet-source.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+# Couldn't compile on Rasberry Pi
+MEMORY=$(free | grep Mem: | awk '{print $2}' )
+SWAP=$(free | grep Swap: | awk '{print $2}' )
+CREATEDSWAP=0
+
+if [[ $MEMORY -lt 2097152 ]]; then
+    echo "Memory smaller then 2GB. Checking swap."
+    if [[ $SWAP -lt 1048576 ]]; then
+        echo "Creating swap in /root/swap."
+        if [[ -e /root/swap ]]; then
+            echo "/root/swap exists!"
+            exit
+        fi
+        dd if=/dev/zero of=/root/swap bs=1024 count=1048576
+        chmod 0600 /root/swap
+        mkswap /root/swap
+        swapon /root/swap
+        CREATEDSWAP=1
+    fi
+fi
+
 cd /root/kismet || exit
 make distclean
 git pull
@@ -11,3 +32,7 @@ make plugins-install
 make restricted-plugins
 make restricted-plugins-install
 
+if [[ "$CREATEDSWAP" == "1" ]]; then                                                                                                                                                                                                          
+    swapoff /root/swap
+    rm /root/swap
+fi
