a119c5120cd8fab61dcf8457e223f1f7b7afc31b
Peter Reuterås <peter@reuteras.com>
2019-12-03 13:14:24 +0100

    Remove --force-yes and don't install ecryptfs-utils

--- bin/setup.sh
+++ bin/setup.sh
@@ -6,16 +6,15 @@ echo "Running apt-get dist-upgrade."
 DEBIAN_FRONTEND=noninteractive APT_LISTCHANGES_FRONTEND=none \
     apt-get \
     -o Dpkg::Options::="--force-confnew --force-confdef" \
-    --force-yes \
     -fuy \
     dist-upgrade
 
 # Install VMware tools
-apt-get install -y open-vm-tools-desktop fuse
+apt install -y open-vm-tools-desktop fuse
 
 # Install tools
 apt-get install -y libbde-dev libbde-utils exfat-fuse exfat-utils \
-    pv ecryptfs-utils
+    pv
 
 # Make sure pip is up to date
 # pip install --upgrade pip
