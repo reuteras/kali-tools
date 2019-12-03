7755d30cb7cb0c18e44a0ce15075187c12988c47
Peter Reuterås <peter@reuteras.com>
2019-12-03 13:28:37 +0100

    Cleanup for new apt.

--- bin/setup.sh
+++ bin/setup.sh
@@ -4,17 +4,13 @@ echo "Running apt-get update."
 apt-get -qq update
 echo "Running apt-get dist-upgrade."
 DEBIAN_FRONTEND=noninteractive APT_LISTCHANGES_FRONTEND=none \
-    apt-get \
-    -o Dpkg::Options::="--force-confnew --force-confdef" \
-    -fuy \
-    dist-upgrade
+    apt -fuy dist-upgrade
 
 # Install VMware tools
 apt install -y open-vm-tools-desktop fuse
 
 # Install tools
-apt-get install -y libbde-dev libbde-utils exfat-fuse exfat-utils \
-    pv
+apt install -y libbde-dev libbde-utils exfat-fuse exfat-utils pv
 
 # Make sure pip is up to date
 # pip install --upgrade pip
