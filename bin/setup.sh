c3b62bccadbcb202e3074068cd6f0460ed8c8e7b
Peter Reuterås <peter@reuteras.com>
2020-09-10 13:05:57 +0200

    Use sudo and clean up.

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 
 echo "Running apt-get update."
-apt-get -qq update
+sudo apt-get -qq update
 echo "Running apt-get dist-upgrade."
 DEBIAN_FRONTEND=noninteractive APT_LISTCHANGES_FRONTEND=none \
-    apt \
+    sudo apt \
         -o Dpkg::Options::=--force-confold \
         -o Dpkg::Options::=--force-confdef \
         -y --allow-downgrades --allow-remove-essential --allow-change-held-packages dist-upgrade
 
 # Install VMware tools
-apt install -y open-vm-tools-desktop fuse
+sudo apt install -y open-vm-tools-desktop fuse
 
 # Install tools
-apt install -y libbde-dev libbde-utils exfat-fuse exfat-utils pv
+sudo apt install -y \
+    exfat-fuse \
+    exfat-utils \
+    libbde-dev \
+    libbde-utils \
+    pv
 
-# Make sure pip is up to date
-# pip install --upgrade pip
-
-# Install tool for Rubber ducky
-# Resources http://usbrubberducky.com/#!resources.md
-# pip install --upgrade ducktoolkit
