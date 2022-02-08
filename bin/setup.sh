02a6284ae0e4b6f5449499f4dd04ed6f550db192
Peter Reuterås <peter@reuteras.com>
2022-02-08 10:59:46 +0100

    Remove already installed tools and add the remaining ones.

--- bin/setup.sh
+++ bin/setup.sh
@@ -9,14 +9,10 @@ DEBIAN_FRONTEND=noninteractive APT_LISTCHANGES_FRONTEND=none \
         -o Dpkg::Options::=--force-confdef \
         -y --allow-downgrades --allow-remove-essential --allow-change-held-packages dist-upgrade
 
-# Install VMware tools
-#sudo apt install -y open-vm-tools-desktop fuse
-
 # Install tools
-#sudo apt install -y \
-#    exfat-fuse \
-#    exfat-utils \
-#    libbde-dev \
-#    libbde-utils \
-#    pv
+sudo apt install -y \
+    exfat-fuse \
+    libbde-dev \
+    libbde-utils \
+    pv
 
